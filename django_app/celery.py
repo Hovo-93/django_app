de7be8c60382140d5c050e7d0cea4b85a0ceb7e4
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2023-11-23 02:55:37 +0300

    added celery settings

--- django_app/celery.py
+++ django_app/celery.py
@@ -2,6 +2,7 @@
 Файл настроек Celery
 https://docs.celeryproject.org/en/stable/django/first-steps-with-django.html
 """
+from __future__ import absolute_import
 import os
 from celery import Celery
 import django
@@ -16,8 +17,11 @@ app = Celery("django_app")
 
 # Для получения настроек Django, связываем префикс "CELERY" с настройкой celery
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.broker_url = settings.CELERY_BROKER_URL
+app.conf.broker_url = 'redis://localhost:6379/0'
 # загрузка tasks.py в приложение django
 app.autodiscover_tasks()
 
 
+@app.task(bind=True)
+def debug_task(self):
+    print('request.'.format(self.request))
