5997613cc19757a0ae8a24ea3025af5515ec7825
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2023-11-23 22:24:39 +0300

    changed broker_url

--- django_app/celery.py
+++ django_app/celery.py
@@ -17,7 +17,7 @@ app = Celery("django_app")
 
 # Для получения настроек Django, связываем префикс "CELERY" с настройкой celery
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.broker_url = 'redis://localhost:6379/0'
+app.conf.broker_url = 'redis://redis:6379/0'
 # загрузка tasks.py в приложение django
 app.autodiscover_tasks()
 
